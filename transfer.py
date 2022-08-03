d3a892f23a3221a9e066427580c364ba5a2b3b0f
seongjungkim <plznw4me@naver.com>
2022-08-04 08:41:34 +0900

    #1 hwp2hwpx 추상 메소드 생성

---
+++ transfer.py
@@ -0,0 +1,17 @@
+class HwpTransfer:
+    def __init__(self):
+        pass
+
+
+    def hwp2hwpx(self, file_path):
+        """
+        Description:
+            hwp 파일을 hwpx 파일로 변환합니다.
+        Args:
+            :param str file_path: hwp 파일 path 을 지정합니다.
+
+        :return str saved_path:
+            hwpx 가 저장된 경로를 반환합니다.
+        """
+        raise NotImplementedError
+        return saved_path
